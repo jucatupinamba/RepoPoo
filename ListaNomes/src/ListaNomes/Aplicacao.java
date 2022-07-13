a1e2535709fd4db70bfd407014a9087ccc7fb979
jucatupinamba <juniorbmx13@gmail.com>
2022-07-13 16:59:17 -0300

    refactor: utilização de função booleana do Scanner

---
+++ ListaNomes/src/ListaNomes/Aplicacao.java
@@ -0,0 +1,30 @@
+package ListaNomes;
+/*
+Criar um programa onde, ao informar um nome, ele deve pesquisar em uma lista já existente o nome informado.
+Ele deve retornar uma msg dizendo se foi encontrado ou não o nome nessa lista.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Aplicacao {
+    public static void main (String [] args) {
+
+        Scanner scan = new Scanner(System.in);
+        List<Processo> lista = new ArrayList<>();
+
+        System.out.println("Digite abaixo nomes para compor a lista: ");
+        System.out.println("Caso deseje encerrar a lista digite um número.");
+
+            for(int i = 0; i <= lista.size(); i++) {
+                System.out.print("Nome: ");
+                if(scan.hasNextLine()) {
+                    Processo processo = new Processo(scan.nextLine());
+                    lista.add(i, processo);
+                }
+            }
+        }
+    }
+
+
--- ListaNomes/src/ListaNomes/Processo.java
+++ ListaNomes/src/ListaNomes/Processo.java
@@ -1,11 +1,11 @@
 package ListaNomes;
+
 public class Processo {
+
     private String nome;
-
-    public Processo() {
-
+    public Processo(String nome){
+        this.nome = nome;
     }
-
     public String getNome() {
         return nome;
     }
